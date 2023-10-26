870bc5d61fc677dba7dd297d0a44aed9320fa57a
Naresh <nareshnikam999@gmail.com>
2023-10-26 11:20:47 +0530

    index file changes

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 require("dotenv").config();
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 const cors = require("cors");
-
-
 const applicationRouter = require("./routes/common.router");
 const testRouter = require("./routes/test.rounter");
 
+// Render Engine
+app.engine('html', require('ejs').renderFile); // Render HTML Templates
+
+app.use(express.json({ limit: '25mb' })); // Limit the response
 app.use(bodyParser.json()); // Parse Body In JSON Format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors()); // Used CORS for cross-origin and allow-same-origin
 
-
+// Routes
 app.use("/api/test", testRouter);
 app.use("/api", applicationRouter);
+
 // Entry API or Welcome Route
-app.get("/", (_req, res) => {
-    res.json({ message: res.__("message") + ` Server is running on port ${PORT}.` });
+app.get("/", (req, res) => {
+    res.json({ message: ` Server is running on port ${PORT}.` });
 });
 
 // Listening PORT
